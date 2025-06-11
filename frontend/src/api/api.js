43f6e20c418d9a2aed0ba2b1590528510d22b054
Denis Bakulin <dtorkon@yandex.ru>
2025-06-11 22:05:01 +0000

    make

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import { getAccessToken, getRefreshToken, setAuthTokens } from './auth'
 
 
 const api = axios.create({
-  baseURL:  'http://localhost:5000/api/' ,
+  baseURL:  '/api/' ,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
@@ -31,7 +31,7 @@ api.interceptors.request.use(config => {
 export const refreshToken = async () => {
   try {
     const refresh_token = getRefreshToken();
-    const response = await axios.post('http://localhost/api/auth/refresh', {}, {
+    const response = await axios.post('/api/auth/refresh', {}, {
       headers: {
         'Authorization': `Bearer ${refresh_token}`
       }
