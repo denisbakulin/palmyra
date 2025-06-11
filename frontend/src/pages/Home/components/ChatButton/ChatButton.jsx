74e57c53305f0ebbbd8b92333690cbe8c60bddb7
Denis Bakulin <dtorkon@yandex.ru>
2025-06-11 20:48:23 +0000

    make

--- frontend/src/pages/Home/components/ChatButton/ChatButton.jsx
+++ frontend/src/pages/Home/components/ChatButton/ChatButton.jsx
@@ -14,14 +14,12 @@ export default function ChatButton ({
         <div className={`chat-button noselect ${isActive ? "active" : ''}`} onClick={onClick}>
             <img className="chat-avatar" src={avatar} alt="avatar"/>
             <div className="chat-filling">
+                <p className="chat-name title">{chatName}</p>
                 <div style={{width: "100%", display: "flex"}}>
-                    
-                    <div className="title">
-                        <span className="chat-name">{chatName}</span>
-                    </div>
                     <p className="last-message">{lastMessage}</p>
+                    <p className="last-message-time">{lastMessageTime}</p>
                 </div>
-                <p className="last-message-time">{lastMessageTime}</p>
+
             </div>
 
         </div>
