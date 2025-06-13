fb5f4262aa74f6ece4748eb9e2a3c4a9dbb5a78b
Denis Bakulin <dtorkon@yandex.ru>
2025-06-13 12:43:11 +0000

    make

--- frontend/src/pages/Home/components/ChatButton/ChatButton.jsx
+++ frontend/src/pages/Home/components/ChatButton/ChatButton.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useRef} from "react"
 import "./ChatButton.css"
 
 
@@ -10,17 +10,22 @@ export default function ChatButton ({
         lastMessageTime,
         avatar
 }) {
+    const fillingRef= useRef()
+    const lastMessageRef= useRef()
+
     return (
        <div className={`chat-button noselect ${isActive ? "active" : ''}`} onClick={onClick}>
           <img className="chat-avatar" src={avatar} alt="avatar" />
-          <div className="chat-filling">
+          <div className="chat-filling" ref={fillingRef}>
               <div className="chat-preview-row">
                <p className="chat-name title">{chatName}</p>
-               <div className="last-message-time">{lastMessageTime}</div>
+               <div className="last-message-time" >{lastMessageTime}</div>
               </div>
-                <div className="last-message">{lastMessage}</div>
-
-  </div>
+              <div className={`last-message ${
+                  lastMessageRef?.current.scrollWidth > fillingRef?.current.clientWidth 
+                  ? "scroll-on-hover" : "" }`} ref={lastMessageRef}>{lastMessage}
+              </div>
+          </div>
 </div>
 
     )
