983b0c3a9a98eb103129705c189a243f1552c3a2
hordaaa <dtorkon@yandex.ru>
2025-06-16 12:48:10 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,14 +18,6 @@ export default defineConfig({
   server: {
     port: 80,
     host: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
-    },
   },
   build: {
     outDir: 'dist',
